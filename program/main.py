cf537bc978c856568017aaf6669d7fb262a72684
4bitFox <33175205+4bitFox@users.noreply.github.com>
2023-07-06 14:23:18 +0200

    Add files via upload

--- program/main.py
+++ program/main.py
@@ -1,4 +1,5 @@
-#!/bin/python3
+#!/usr/bin/env python3
+
 
 from time import sleep
 import ui
--- program/read.py
+++ program/read.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 ## Reads RAW file and displays it.
 import os.path
 import sys
