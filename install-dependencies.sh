0aeb1ce76b2e6a11340f44c80644c75c5412f386
4bitFox <33175205+4bitFox@users.noreply.github.com>
2023-07-07 13:38:52 +0200

    Add files via upload

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
-su
 
 print "Installing required packages..."
-apt install python3 python-tk python3-smbus i2c-tools
+sudo apt install python3 python-tk python3-smbus i2c-tools
 
 print "Installing python libraries..."
-pip install --upgrade adafruit-circuitpython-mlx90640 adafruit-blinka RPI.GPIO numpy matplotlib configparser PySimpleGUI
+sudo pip install --upgrade adafruit-circuitpython-mlx90640 adafruit-blinka RPI.GPIO numpy matplotlib configparser PySimpleGUI
