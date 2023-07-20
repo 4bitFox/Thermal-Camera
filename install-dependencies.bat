a5b588ec6277dd5de5930b5b3b48d1a73be05021
4bitFox <33175205+4bitFox@users.noreply.github.com>
2023-07-20 10:19:29 +0200

    Update install-dependencies.bat

--- install-dependencies.bat
+++ install-dependencies.bat
@@ -4,11 +4,11 @@ echo This script will guide you to install everything you need to run the Viewer
 pause
 cls
 
-echo You will now install Python 3 from the Microsoft Store.
-echo I will open the Microsoft Store for you. Install the newest stable version of Python 3 :-)
+echo You will now install Python 3.9 from the Microsoft Store.
+echo I will open the Microsoft Store for you. Install Python 3.9 :-)
 echo After Python 3 finished installing, close the Microsoft Store and return to this window.
 echo If you have Python 3 already installed you can skip this step.
-echo Alternatively you can also use the regular Python 3 installer if you so wish. In that case skip this step.
+echo Alternatively you can also use the regular Python 3.9.2 installer if you so wish. In that case skip this step.
 echo To skip this step, close the Microsoft Store after it opens and return to this window.
 pause
 explorer "ms-windows-store://publisher/?name=Python Software Foundation"
